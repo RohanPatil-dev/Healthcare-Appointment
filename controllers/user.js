17b4f899993f36cb0b099b85749dd3e3b6290b05
Rohan Patil <rohanpatil5122001@gmail.com>
2024-06-15 22:55:35 +0530

    Minor changes in login controller function

--- controllers/user.js
+++ controllers/user.js
@@ -4,49 +4,48 @@ const bcrypt = require("bcryptjs")
 
 const { setUser } = require("../Services/services")
 
-async function register(req,res) {
- try {
-    const register = new user(req.body)
+async function register(req, res) {
+    try {
+        const register = new user(req.body)
 
-    await register.save()
+        await register.save()
 
-    return res.json(register)
- } catch (error) {
-    return res.json({error : error})
- }
+        return res.json(register)
+    } catch (error) {
+        return res.json({ error: error })
+    }
 }
 
-async function login(req,res){
-   try {
+async function login(req, res) {
+
     const { email, password } = req.body
 
-    const login = await user.findOne({ email, password })
+    try {
+        const login = await user.findOne({ email, password })
 
-    if (!login)
-        return res.json({
-            error: "Invalid Email or Password",
-        });
+        if (login) {
+            return res.json(login)
+        } else {
+            return res.json({
+                msg : "Invalid Email or Password",
+            })
+        };
 
 
-    const token = setUser(login)
 
-    res.cookie("uid", token)
-
-    console.log(token);
-    return res.json({msg : login,token : token})
-   } catch (err) {
-     return res.json({err : err})
-   }
+    } catch (err) {
+        return res.json({ msg: "server error" })
+    }
 }
 
 async function allPatient(req, res) {
     try {
         const getAllData = await user.find()
 
-    return res.json(getAllData)
+        return res.json(getAllData)
     } catch (error) {
-        return res.json({error : error})
+        return res.json({ error: error })
     }
 }
 
-module.exports = {register,login,allPatient}
+module.exports = { register, login, allPatient }
